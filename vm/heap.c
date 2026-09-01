99f1099696a58b1c549651a259b013c648a0f2be
nayoungwook <nayoungwook@kaist.ac.kr>
2026-09-01 15:04:58 +0900

    vm heap

---
+++ vm/heap.c
@@ -0,0 +1,6 @@
+#include <heap.h>
+#include <vm.h>
+
+void vm_malloc(struct VM *vm, unsigned size){
+    
+}
---
+++ vm/include/heap.h
@@ -0,0 +1,8 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+struct VM;
+
+void vm_malloc(struct VM *vm, unsigned size);
+
+#endif
