25a12aaf747b65ca53ad8e816318ceee6f705ff4
nayoungwook <nayoungwook@kaist.ac.kr>
2026-09-16 04:48:58 +0900

    heap view problem fixed.

--- vm/heap.c
+++ vm/heap.c
@@ -12,32 +12,66 @@
 */
 
 #ifdef DEBUG
-static void debug_print_heap(struct VM *vm) {
-     unsigned i;
+static bool is_live_heap_block(const struct VM *vm, const void *block) {
+  unsigned i;
 
-     printf("\nheap view\n");
-     for (i = 1; i < vm->heap_index; i++) {
-          void *heap = vm->heap_mapper[i];
+  for (i = 1; i < vm->heap_index; i++) {
+    if (vm->heap_mapper[i] == block)
+      return true;
+  }
+  return false;
+}
 
-          if(heap != NULL){
-              uint64_t header;
-              memcpy(&header, heap, sizeof(header));
-              printf("[ id : %d | %d byte ]",
-                    (uint32_t) header, (uint32_t) (header >> 32));
-          } else {
-               printf("[ FREE ]");
-          }
-     }
-     printf("\n");
+static void debug_print_heap_view(const struct VM *vm) {
+  const uint8_t *block = (const uint8_t *)vm->heap;
+  const uint8_t *heap_end = (const uint8_t *)vm->heap_alloc_loc;
+
+  printf("\nheap view\n");
+  while (block < heap_end) {
+    uint64_t header;
+    uint32_t object_id;
+    uint32_t size;
+    size_t remaining = (size_t)(heap_end - block);
+
+    if (remaining < HEAP_META_SIZE) {
+      printf("[ CORRUPT | %zu byte ]", remaining);
+      break;
+    }
+
+    memcpy(&header, block, sizeof(header));
+    object_id = (uint32_t)header;
+    size = (uint32_t)(header >> 32);
+
+    if ((size_t)size > remaining - HEAP_META_SIZE) {
+      printf("[ CORRUPT | size : %" PRIu32 " byte ]", size);
+      break;
+    }
+
+    if (is_live_heap_block(vm, block))
+      printf("[ id : %" PRIu32 " | %" PRIu32 " byte ]", object_id, size);
+    else
+      printf("[ FREE | %" PRIu32 " byte ]", size);
+
+    block += HEAP_META_SIZE + size;
+  }
+  printf("\n");
 }
 #endif
 
 void vm_free(struct VM *vm, unsigned heap_mapper_index) {
+  if (heap_mapper_index == 0 || heap_mapper_index >= vm->heap_index ||
+      vm->heap_mapper[heap_mapper_index] == NULL)
+    return;
+
   unsigned *hmi = (unsigned *)S_malloc(sizeof(unsigned));
   *hmi = heap_mapper_index;
   q_push(vm->heap_index_queue, hmi);
 
   vm->heap_mapper[heap_mapper_index] = NULL;
+
+#ifdef DEBUG
+  debug_print_heap_view(vm);
+#endif
 }
 
 unsigned vm_malloc(struct VM *vm, unsigned size, int object_id) {
@@ -68,7 +102,7 @@ unsigned vm_malloc(struct VM *vm, unsigned size, int object_id) {
   vm->heap_alloc_loc = (uint8_t *)vm->heap_alloc_loc + size + HEAP_META_SIZE;
 
 #ifdef DEBUG
-    debug_print_heap(vm);
+  debug_print_heap_view(vm);
 #endif
   
   return heap_mapper_index;
